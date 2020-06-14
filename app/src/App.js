e5257e3fb4a7b4b0e282fbc898f1f6e96fa08e08
fabriciomaruta <fabricio.maruta@gmail.com>
2020-06-14 20:17:20 -0300

    add router to project

--- app/src/App.js
+++ app/src/App.js
@@ -2,25 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {
+	  BrowserRouter as Router,
+		  Switch,
+		  Route,
+		  Link
+} from "react-router-dom";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <div>FALAE QUE ESSA AQUI EH A HOME</div>
+                </Route>
+                <Route exact path="/about">
+                    <div>SOBRE NOIS !</div>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
