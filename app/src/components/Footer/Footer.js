2bc864278c384a8c84c1bdf2131010e5069f8808
Paulo Pereira <>
2021-03-28 17:07:23 -0300

    Footer update

--- app/src/components/Footer/Footer.js
+++ app/src/components/Footer/Footer.js
@@ -1,47 +1,3 @@
-// import React from 'react';
-// import {Grid, Row, Col} from 'react-bootstrap';
-// export default function Footer() {
-//     return (
-//         <div style={{backgroundColor:'#e4482e', bottom:'0px', position:'absolute', width: '100%', height:'100px'}}>
-//             {
-//             // <Row>
-//             //     <Col sm={1} style={{backgroundColor:'black'}}>af
-//             //     </Col>
-//             //     <Col sm={1} style={{backgroundColor:'white'}}>asdfghjkqwertyuiopl
-//             //     </Col>
-//             //     <Col sm={1} style={{backgroundColor:'black'}}>k
-//             //     </Col>
-//             //     <Col sm={1} style={{backgroundColor:'white'}}>k
-//             //     </Col>
-//             //     <Col sm={1} style={{backgroundColor:'black'}}>j
-//             //     </Col>
-//             //     <Col sm={1} style={{backgroundColor:'white'}}>h
-//             //     </Col>
-//             //     <Col sm={1} style={{backgroundColor:'black'}}>g
-//             //     </Col>
-//             //     <Col sm={1} style={{backgroundColor:'white'}}>i
-
-//             //     </Col>
-//             //     <Col sm={1} style={{backgroundColor:'black'}}>
-//             //         asd
-//             //     </Col>
-//             //     <Col sm={1} style={{backgroundColor:'white'}}>
-//             //     i
-//             //     </Col>
-//             //     <Col sm={1} style={{backgroundColor:'black'}}>
-//             //     b
-//             //     </Col>
-//             //     <Col sm={1} style={{backgroundColor:'red'}}>
-//             //     f
-//             //     </Col>
-//             // </Row>
-
-//             }
-//                 Footer Infos
-//         </div>
-//     );
-// }
-
 import React from "react";
 import {
     Box,
@@ -53,32 +9,53 @@ import {
     Heading,
 } from "./FooterStyles";
 
+import backgroungImage from '../../assets/Footer/footer-background.png';
+import { Image } from "react-bootstrap";
+
 const Footer = () => {
     return (
-        <Box>
+        <Box style={{ background: backgroungImage, position: "relative" }}>
+            <div style={{ width: "100%", height: "100%", opacity: 0.7, position: "absolute", zIndex: -2 }}>
+                <Image src={backgroungImage} style={{ width: "100%", height: "100%" }}></Image>
+            </div>
+            <div style={{
+                width: "100%", height: "100%", opacity: 0.6, 
+                position: "absolute",
+                zIndex: -1,
+                backgroundColor: "black"
+            }}>
+
+            </div>
             <Container>
-                <Row>
-                    <Column>
+                <Row className='row'>
+                    <Column className="col-2">
                         <Heading>Sobre Nós</Heading>
                         <FooterLink href="#">Empresa</FooterLink>
                     </Column>
-                    <Column>
+                    <Column className="col-3">
                         <Heading>Serviços</Heading>
                         <FooterLink href="#">Wire-O</FooterLink>
                         <FooterLink href="#">Espirais de Aço</FooterLink>
                         <FooterLink href="#">Espirais de PVC</FooterLink>
                     </Column>
-                    <Column>
-                        <Heading>Contate nos</Heading>
+                    <Column className="col-3">
+                        <Heading>Contato</Heading>
                         <FooterItem>veredaacabementos@vereda.com.br</FooterItem>
-                        <FooterItem>Telefones:11 3858-2359
-                            {
-                            /* <ul>11 3858-2359</ul>
-                            <ul>11 2501-0882</ul>
-                            <ul>11 97468-5433 (WhatsApp)</ul> */
-                            }
+                        <FooterItem>Telefones:11 3858-2359<br></br>11 2501-0882<br></br>
+                            <a href="https://wa.me/+5511974685433">
+                                WhatsApp</a>
                         </FooterItem>
-                        <FooterItem>Rua Félix Guilhem, 940 Lapa de Baixo - São Paulo-SP Cep: 05069-000</FooterItem>
+                    </Column>
+                    <Column className="col-3">
+                        <Heading>Localização</Heading>
+                        <FooterItem>Rua Félix Guilhem, 940<br></br>
+                        Lapa de Baixo<br></br>
+                        São Paulo-SP <br></br>
+                        Cep: 05069-000
+                        </FooterItem>
+                    </Column>
+                    <Column className="col-1">
+                        <Heading>Siga-nos</Heading>
                         <FooterLink href="https://br.linkedin.com/company/vereda-acabamentos-gr%C3%A1ficos">
                             <i className="fab fa-linkedin">
                                 <span style={{ marginLeft: "0px" }}>
--- app/src/components/Footer/FooterStyles.js
+++ app/src/components/Footer/FooterStyles.js
@@ -1,9 +1,7 @@
 import styled from 'styled-components';
 
 export const Box = styled.div`
-  padding: 30px 40px;
-  background: black;
-  position: fixed;
+  /* padding: 30px 40px; */
   left:0;
   bottom:0;
   right:0;
@@ -15,32 +13,36 @@ export const Box = styled.div`
 `;
 
 export const Container = styled.div`
+    padding: 30px 40px;
     display: flex;
     flex-direction: column;
     justify-content: center;
-    max-width: 1000px;
+    max-width: 100%;
     margin: 0 auto;
-    padding: 20px;
-    background: green;
-`
+    /* padding: 20px; */
+    /* background: green; */
+`;
 
 export const Column = styled.div`
   display: flex;
   flex-direction: column;
   text-align: left;
   margin-left: 0px;
-  background: red
+  /* background: red; */
+  padding: 0 10px 0 10px;
 `;
 
 export const Row = styled.div`
-  display: grid;
+display: flex;
+flex-direction: row;
+/* display: grid;
   grid-template-columns: repeat(auto-fill, 
-                         minmax(185px, 1fr));
-  grid-gap: 20px;
+                         minmax(185px, 1fr)); */
+  /* grid-gap: 20px;
   @media (max-width: 1000px) {
     grid-template-columns: repeat(auto-fill, 
                            minmax(200px, 1fr));
-  }
+  } */
 `;
 
 export const FooterLink = styled.a`
@@ -59,12 +61,13 @@ export const FooterItem = styled.p`
   color: #fff;
   margin-bottom: 20px;
   font-size: 18px;
+  width: 100%;
+  word-break:break-all;
   text-decoration: none;
-   
-//   &:hover {
-//       color: #F89C29;
-//       transition: 200ms ease-in;
-  }
+     /* &:hover {
+      color: #F89C29;
+     transition: 200ms ease-in;
+  } */
 `;
 
 export const Heading = styled.p`
